4fd6d97f124395a5a0ea1f38eb1db267ad63c55d
Narendra Jaggi <jagginarendra@gmail.com>
2017-03-28 17:29:21 +0530

    controller with  multiple query param for list

--- InventoryServiceWithRedisCache/src/main/java/com/bjs/controller/InventoryController.java
+++ InventoryServiceWithRedisCache/src/main/java/com/bjs/controller/InventoryController.java
@@ -7,6 +7,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bjs.model.Inventory;
@@ -14,22 +15,34 @@ import com.bjs.service.InventoryService;
 
 @SuppressWarnings("unused")
 @RestController
-
 public class InventoryController {
 
 	@Autowired
 	private InventoryService inventoryService;
 
-	private static final Logger LOGGER =  LoggerFactory.getLogger(InventoryController.class);
-	
-	
+	private static final Logger LOGGER = LoggerFactory.getLogger(InventoryController.class);
+
 	@RequestMapping("/inventory/sku/{skuId}")
-	//@Cacheable("getInventoryBySku")
-	public Inventory getInventoryBySku(@PathVariable String skuId ) {
+	// @Cacheable("getInventoryBySku")
+	public Inventory getInventoryBySku(@PathVariable String skuId) {
 
 		LOGGER.info("InventoryMS InventoryController's skuId=" + skuId);
 
 		return inventoryService.getInventoryDetails(skuId);
+
+	}
+
+	@RequestMapping("/inventory/sku/list")
+	// @Cacheable("getInventoryBySku")
+	public void getInventoryBySkuList(@RequestParam("sku") String[] skuArray) {
+
+		LOGGER.info("InventoryMS InventoryController's skuId=" + skuArray);
+
+		int length = skuArray.length;
+		for (int i = 0; i < skuArray.length; i++) {
+			LOGGER.info(skuArray[i]);
+		}
+		// return inventoryService.getInventoryDetails(skuId);
 
 	}
 
